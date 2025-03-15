4010aa66a0881e7755d7734ed1845055d38ec26e
hykocode <protocolmaya@gmail.com>
2025-03-15 18:56:48 +0800

    modify index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,11 +28,12 @@
             <span>r</span>
             <span>i</span>
             <span>k</span>
-            &nbsp;<span>!</span>
+            &nbsp;
+            <span>!</span>
             </div></h1>
             
             
-                <button id="registerBtn" class=" px-6 py-3 rounded-lg text-lg">Daftar Sekarang!</button>
+                <button id="registerBtn" class=" px-6 py-3 rounded-lg text-lg "  ><p >Daftar Sekarang!</p></button>
             
         </div>
     </section>
@@ -57,7 +58,7 @@
                     <label for="loginPassword" class="block text-gray-700 mb-2">Password</label>
                     <input type="password" id="loginPassword" name="password" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500" required>
                 </div>
-                <button type="submit" class="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition">Login</button>
+                <button type="submit" class="w-full  text-white py-2 rounded-lg  transition">Login</button>
             </form>
             <div class="mt-4 text-center">
                 <p>Don't have an account? <button id="switchToRegister" class="text-indigo-600 hover:underline">Register</button></p>
@@ -101,7 +102,7 @@
                         </div>
                     </div>
                     <button type="submit" class="w-full bg-indigo-600 text-white py-2 rounded-lg hover:bg-indigo-700 transition">Verify</button>
-                    <button type="button" id="resendOtp" class="w-full mt-2 bg-white text-indigo-600 border border-indigo-600 py-2 rounded-lg hover:bg-indigo-50 transition">Resend Code</button>
+                    <button type="button" id="resendOtp" class="w-full mt-2 bg-white text-white border border-indigo-600 py-2 rounded-lg hover:bg-indigo-50 transition">Resend Code</button>
                 </form>
             </div>
             
@@ -133,4 +134,5 @@
     </div>
 
     <div class="bgimg"><img src="{{asset('images/background-masjid-01.svg')}}" alt=""></div>
+
     @include('footer')
